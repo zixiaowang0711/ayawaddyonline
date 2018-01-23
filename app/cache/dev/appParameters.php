6fa3bf7dfbee0f733f0fa1084d3066fc2a3076fe
Thandah <rothandaraung1@gmail.com>
2018-01-23 16:43:26 +0630

    just updated css text changed the color for testing

---
+++ app/cache/dev/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'ayawaddyonline',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'j2TxXSXKi1nsoZRmEKOLs19abePLSzgHRjBjKIJewReUe5ymAifIMTDe',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-11-06',
+    'locale' => 'en-US',
+    'cookie_key' => '7vyXdKpDI9ywJzTXMRfiQ7bUHsw2QQ9k5o2Ot7wy8gLjJiheM9CfAfzn',
+    'cookie_iv' => 'SwAoWn9z',
+    'new_cookie_key' => 'def00000a79396362afa7e874aed8dbd3b3eab47fc0e29193b41d3ef43f0158d63e1ecf14a40a4241ffc23567d4fbb23619fdbf8e43121d25897c56cc62954b58ec9b959',
+  ),
+);
